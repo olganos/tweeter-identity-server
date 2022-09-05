49e52fbef24834b301cb19bb2e400b19dd88d2c9
Olga Nosova <nosova.olgs@gmail.com>
2022-09-05 14:28:04 +0200

    used duende server

---
+++ IdentityServerAspNetIdentity/Services/DummyEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace IdentityServerAspNetIdentity.Services;
+
+public class DummyEmailSender : IEmailSender
+{
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        return;
+    }
+}
